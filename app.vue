4932f022f51d73d5edf838b798257eb618868908
Luis Velásquez <luvejo.dev@gmail.com>
2023-09-09 10:19:04 -0500

    build: add initial commit with bare Nuxt 3 scaffold

---
+++ app.vue
@@ -0,0 +1,3 @@
+<template>
+  <div> Hello, Friend. </div>
+</template>
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  arrowParens: 'always',
+  singleQuote: true,
+  endOfLine: 'auto',
+  printWidth: 80,
+  trailingComma: 'all',
+  htmlWhitespaceSensitivity: 'strict',
+}
