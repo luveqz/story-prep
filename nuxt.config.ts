2aee4db24258b99e9d7fdc7f496f5ec0db006794
Luis Velásquez <luvejo.dev@gmail.com>
2023-09-09 10:32:21 -0500

    build: setup Tailwind and Prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  css: ['@/assets/css/main.css'],
+
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
 })
--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
   semi: false,
   arrowParens: 'always',
   singleQuote: true,
