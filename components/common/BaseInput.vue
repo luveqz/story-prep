0eb96f35ac89051b56fce0875d502c6ea6b0ac3d
Luis Velásquez <luvejo.dev@gmail.com>
2023-09-10 20:28:30 -0500

    chore: add v-model prop to custom input components

--- components/common/BaseInput.vue
+++ components/common/BaseInput.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+defineEmits(['update:modelValue'])
+
+defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
 <template>
   <input
     class="rounded-md bg-brown-50 p-4 text-brown-700 shadow-[0px_2px_0px_0px_--tw-shadow] shadow-brown-100"
+    :value="modelValue"
+    @input="
+      $emit('update:modelValue', ($event.target as HTMLInputElement).value)
+    "
   />
 </template>
