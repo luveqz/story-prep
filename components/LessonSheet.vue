c02a8e93e1b99b4e9d8177faa4c23f6b09f5f71f
Luis Velásquez <luvejo.dev@gmail.com>
2023-09-11 09:43:29 -0500

    feat: add "Save as PDF" button and logic

--- components/LessonSheet.vue
+++ components/LessonSheet.vue
@@ -1,49 +1,88 @@
+<script setup lang="ts">
+import domtoimage from 'dom-to-image'
+import { jsPDF } from 'jspdf'
+
+const lesson = ref<HTMLElement | undefined>()
+const capturing = ref(false)
+
+const saveAsPdf = async () => {
+  if (lesson.value) {
+    const scale = 2
+    const { width, height } = lesson.value.getBoundingClientRect()
+
+    capturing.value = true
+    const blob = await domtoimage.toPng(lesson.value, {
+      width: width * scale,
+      height: height * scale,
+      style: {
+        transform: `scale(${scale})`,
+        transformOrigin: 'top left',
+      },
+    })
+    capturing.value = false
+
+    const outputScale = 0.4
+    const pdf = new jsPDF('p', 'px', [
+      width * outputScale,
+      height * outputScale,
+    ])
+    pdf.addImage(blob, 'PNG', 0, 0, width * outputScale, height * outputScale)
+
+    pdf.save('test.pdf')
+  }
+}
+</script>
 <template>
   <div
-    class="max-h-[calc(100vh_-_2rem)] max-w-[40rem] grow overflow-auto rounded-md border-r-8 border-brown-700 bg-brown-700 p-8"
-    @submit.prevent
+    class="max-h-[calc(100vh_-_2rem)] max-w-[40rem] grow overflow-auto rounded-md border-r-8 border-brown-700 bg-brown-700"
   >
-    <header class="mb-10 text-center">
-      <p class="font-londrina text-2xl">English Lesson: </p>
-      <HeadingOne>Why aren’t you laughing</HeadingOne>
-      <p class="text-xl font-medium opacity-60">by David Sedaris</p>
-    </header>
+    <div ref="lesson" class="bg-brown-700 p-8" @submit.prevent>
+      <header class="mb-10 text-center">
+        <p class="font-londrina text-2xl">English Lesson: </p>
+        <HeadingOne>Why aren’t you laughing</HeadingOne>
+        <p class="text-xl font-medium opacity-60">by David Sedaris</p>
+      </header>
 
-    <div class="flex flex-col gap-y-12">
-      <LessonSheetSection
-        v-if="$app.lesson?.verbs"
-        title="Verbs"
-        color-class="bg-blue-500"
-        :entries="$app.lesson?.verbs"
-      />
+      <div class="flex flex-col gap-y-12">
+        <LessonSheetSection
+          v-if="$app.lesson?.verbs"
+          title="Verbs"
+          color-class="bg-blue-500"
+          :entries="$app.lesson?.verbs"
+        />
 
-      <LessonSheetSection
-        v-if="$app.lesson?.nouns"
-        title="Nouns"
-        color-class="bg-green-600"
-        :entries="$app.lesson?.nouns"
-      />
+        <LessonSheetSection
+          v-if="$app.lesson?.nouns"
+          title="Nouns"
+          color-class="bg-green-600"
+          :entries="$app.lesson?.nouns"
+        />
 
-      <LessonSheetSection
-        v-if="$app.lesson?.idioms"
-        title="Idioms"
-        color-class="bg-orange-400"
-        :entries="$app.lesson?.idioms"
-      />
+        <LessonSheetSection
+          v-if="$app.lesson?.idioms"
+          title="Idioms"
+          color-class="bg-orange-400"
+          :entries="$app.lesson?.idioms"
+        />
 
-      <section v-if="$app.lesson?.grammar">
-        <HeadingTwo class="mb-6 text-center">Grammar Topics</HeadingTwo>
+        <section v-if="$app.lesson?.grammar">
+          <HeadingTwo class="mb-6 text-center">Grammar Topics</HeadingTwo>
 
-        <ul class="flex flex-wrap justify-center gap-5">
-          <li
-            v-for="(topic, id) in $app.lesson.grammar"
-            :key="id"
-            class="rounded-md border-[3px] border-dashed border-white/10 px-4 py-3 font-londrina text-xl font-light tracking-wide"
-          >
-            {{ topic }}
-          </li>
-        </ul>
-      </section>
+          <ul class="flex flex-wrap justify-center gap-5">
+            <li
+              v-for="(topic, id) in $app.lesson.grammar"
+              :key="id"
+              class="whitespace-nowrap rounded-md border-[3px] border-dashed border-white/10 px-4 py-3 font-londrina text-xl font-light tracking-wide"
+            >
+              {{ topic }}
+            </li>
+          </ul>
+        </section>
+
+        <BaseButton v-if="!capturing" @click="saveAsPdf">
+          Save as PDF
+        </BaseButton>
+      </div>
     </div>
   </div>
 </template>
