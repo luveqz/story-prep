7806407cfb9129b5ca4124760cc8895322f3d9f7
Luis Velásquez <luvejo.dev@gmail.com>
2023-09-12 16:13:31 -0500

    chore: remove no longer used code

--- stores/app.ts
+++ stores/app.ts
@@ -1,8 +1,4 @@
 import { defineStore } from 'pinia'
-
-import { PALM_API_BASE_URL } from '@/lib/constants'
-import { PALM_API_SAFETY_SETTINGS } from '@/lib/constants'
-import { PROMPT_TEMPLATE } from '@/lib/constants'
 import type { Lesson } from '@/lib/schemas'
 
 export const useAppStore = defineStore({
@@ -26,18 +22,4 @@ export const useAppStore = defineStore({
       }
       lesson: { title: string; author: string; content: Lesson } | undefined
     },
-
-  actions: {
-    generateLesson() {
-      return $fetch(`${PALM_API_BASE_URL}?key=${this.apiKey}`, {
-        method: 'post',
-        body: {
-          prompt: {
-            text: PROMPT_TEMPLATE.replace('{{ STORY }}', this.story.content),
-          },
-          safetySettings: PALM_API_SAFETY_SETTINGS,
-        },
-      })
-    },
-  },
 })
