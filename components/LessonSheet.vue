edc44c8c901b471fbcfb5b3079dcdd32096b2009
Luis Velásquez <luvejo.dev@gmail.com>
2023-09-13 17:10:33 -0500

    docs: fix TypeScript error by checking if object is undefined

--- components/LessonSheet.vue
+++ components/LessonSheet.vue
@@ -38,42 +38,47 @@ const saveAsPdf = async (filename: string) => {
     data-testid="lesson-sheet"
     class="max-h-[calc(100vh_-_2rem)] max-w-[40rem] grow overflow-auto rounded-md border-r-8 border-brown-700 bg-brown-700"
   >
-    <div ref="lesson" class="bg-brown-700 p-8" @submit.prevent>
+    <div
+      v-if="$app.lesson"
+      ref="lesson"
+      class="bg-brown-700 p-8"
+      @submit.prevent
+    >
       <header class="mb-10 text-center">
         <p class="font-londrina text-xl sm:text-2xl">English Lesson: </p>
 
         <HeadingOne>
-          {{ $app.lesson?.title || 'Untitled Story' }}
+          {{ $app.lesson.title || 'Untitled Story' }}
         </HeadingOne>
 
         <p class="text-lg font-medium opacity-60 sm:text-xl">
-          by {{ $app.lesson?.author || 'Anonymous' }}
+          by {{ $app.lesson.author || 'Anonymous' }}
         </p>
       </header>
 
       <div class="flex flex-col gap-y-12">
         <LessonSheetSection
-          v-if="$app.lesson?.content.verbs"
+          v-if="$app.lesson.content.verbs"
           title="Verbs"
           color-class="bg-blue-500"
-          :entries="$app.lesson?.content.verbs"
+          :entries="$app.lesson.content.verbs"
         />
 
         <LessonSheetSection
-          v-if="$app.lesson?.content.nouns"
+          v-if="$app.lesson.content.nouns"
           title="Nouns"
           color-class="bg-green-600"
-          :entries="$app.lesson?.content.nouns"
+          :entries="$app.lesson.content.nouns"
         />
 
         <LessonSheetSection
-          v-if="$app.lesson?.content.idioms"
+          v-if="$app.lesson.content.idioms"
           title="Idioms"
           color-class="bg-orange-400"
-          :entries="$app.lesson?.content.idioms"
+          :entries="$app.lesson.content.idioms"
         />
 
-        <section v-if="$app.lesson?.content.grammar">
+        <section v-if="$app.lesson.content.grammar">
           <HeadingTwo class="mb-6 text-center">Grammar Topics</HeadingTwo>
 
           <ul class="flex flex-wrap justify-center gap-5">
