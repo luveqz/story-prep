a9129c6c767d2f326308880962f511cccdd0fa2b
Luis Velásquez <luvejo.dev@gmail.com>
2024-01-14 10:33:23 -0500

    chore: temporarily remove Cypress from the project to unlock Vercel builds

--- cypress/utils/file.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Delete the downloads folder to make sure the test has "clean"
- * slate before starting.
- */
-export const deleteDownloadsFolder = () => {
-  const downloadsFolder = 'cypress/downloads'
-
-  cy.task('deleteFolder', downloadsFolder)
-}
