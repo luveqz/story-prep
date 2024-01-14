b2159491a5c9bd3d089c7420bae7ff1099860f98
Luis Velásquez <luvejo.dev@gmail.com>
2024-01-14 09:51:34 -0500

    chore: use standard resolution

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     viewportWidth: 1024,
-    viewportHeight: 660,
+    viewportHeight: 768,
 
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.ts')(on, config)
